9e902e0c7348c049404f8505d1137eb1f138732d
sirhmvfx <sirhmvfx@gmail.com>
2024-01-23 10:30:33 +0100

    fixing issues

--- app/(owner)/(auth)/signin/page.jsx
+++ app/(owner)/(auth)/signin/page.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 
 import { doc, getDoc } from "firebase/firestore";
 import { auth, db } from "@/app/firebase/config";
+import { IoEyeOffSharp, IoEyeSharp } from "react-icons/io5";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
 
